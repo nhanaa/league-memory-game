dc7f08e5e746990ad2cf8b24cb2a626c762409dc
nhanaa <nhanpro7d@gmail.com>
2022-12-02 20:07:52 +0000

    Created home game component and modify App.js

--- src/App.js
+++ src/App.js
@@ -1,98 +1,14 @@
 import './App.css';
-import {useEffect, useState} from 'react';
-import Card from './components/Card';
+// import {useEffect, useState} from 'react';
 import Navbar from './components/Navbar';
-import useSound from 'use-sound';
-
-const cardImages = [
-  {"src" : "/img/jinx.png", matched: false},
-  {"src" : "/img/caitlyn.png", matched: false},
-  {"src" : "/img/blitzcrank.png", matched: false},
-  {"src" : "/img/kindred.png", matched: false},
-  {"src" : "/img/taric.png", matched: false},
-  {"src" : "/img/teemo.png", matched: false},
-  {"src" : "/img/lulu.png", matched: false},
-  {"src" : "/img/zoe.png", matched: false},
-]
+// import useSound from 'use-sound';
+import Home from './Home';
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
-
-  const [play, {stop}] = useSound(process.env.PUBLIC_URL + "/audio/happy-journey.mp3");
-
-  // shuffle cards
-  const shuffleCards = () => {
-    stop();
-    play();
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(() => Math.random() - 0.5)
-      .map((card) => ({...card, id: Math.random()}));
-
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setCards(shuffledCards);
-    setTurns(0);
-  }
-
-  // handle a choice
-  const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  }
-
-  // start the game automatically
-  useEffect(() => {
-    // shuffleCards();
-  }, [])
-
-  // compare 2 selected cards
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      setDisabled(true);
-      if (choiceOne.src === choiceTwo.src) {
-        setCards(prevCards => {
-          return prevCards.map(card => {
-            if (card.src === choiceOne.src) {
-              return {...card, matched: true};
-            } else {
-              return card;
-            }
-          })
-        })
-        resetTurn();
-      } else {
-        setTimeout(() => resetTurn(), 1000);
-      }
-    }
-  }, [choiceOne, choiceTwo])
-
-  // reset choices and increase turn
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns(prevTurns => prevTurns + 1);
-    setDisabled(false);
-  }
-
   return (
-    <div>
+    <div className="App">
       <Navbar />
-      <button className="new-game" onClick={shuffleCards}>New Game</button>
-      <div className="card-grid">
-        {cards.map(card => (
-          <Card 
-            key={card.id}
-            card={card}
-            handleChoice={handleChoice}
-            flipped={card === choiceOne || card === choiceTwo || card.matched}
-            disabled={disabled}
-          />
-        ))}
-      </div>
-      <p className="turns">Turns: {turns}</p>
+      <Home />
     </div>
   );
 }

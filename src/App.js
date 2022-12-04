469ae7cd155eba4c2620c70474f8527df9044125
nhanaa <nhanpro7d@gmail.com>
2022-12-04 08:52:15 +0000

    Changed to hash router instead for deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import {useEffect, useState} from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import useSound from 'use-sound';
 import Home from './Home';
